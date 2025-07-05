03d456c0867c470e28f5b3a4f819a5eee08cc5fb
Ahmad AbdEl-Aal <ahmad.abdelaallabas@gmail.com>
2025-07-05 08:31:18 +0300

    refactor: remove unused client creator

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -6,8 +6,3 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createPagesBrowserClient<Database>();
-
-export const getServiceRoleClient = () => {
-  const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
-  return createClient<Database>(supabaseUrl, supabaseServiceKey);
-};
