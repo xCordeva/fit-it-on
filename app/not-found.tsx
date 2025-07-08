fa0ca860a7eadb6038355c0778a6f0c33570f6eb
Ahmad AbdEl-Aal <ahmad.abdelaallabas@gmail.com>
2025-07-08 09:50:00 +0300

    refactor: change go to routing to homepage to route to previous page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,11 @@
-import Image from "next/image";
+"use client";
+import { useRouter } from "next/navigation";
 
 export default function NotFound() {
+  const router = useRouter();
   return (
     <main className="flex flex-col items-center justify-center min-h-screen text-center bg-white px-4">
-      <Image
-        src={"/logo.png"}
-        alt="fititon=logo"
-        height={100}
-        width={100}
-        className="h-15 w-auto mb-10"
-      ></Image>
+      <img src={"/logo.png"} alt="fititon=logo" className="h-15 w-auto mb-10" />
       <h1 className="text-6xl md:text-8xl font-extrabold text-gray-800">404</h1>
       <p className="mt-4 text-xl md:text-2xl text-gray-600">
         Oops! Looks like you’ve lost your way.
@@ -17,12 +13,12 @@ export default function NotFound() {
       <p className="mt-2 text-md text-gray-500">
         The page you’re looking for doesn’t exist.
       </p>
-      <a
-        href="/"
-        className="mt-6 px-6 py-3 bg-primary text-white rounded hover:bg-primary/90 transition"
+      <button
+        onClick={() => router.back()}
+        className="mt-6 px-6 py-3 bg-primary text-white rounded hover:bg-primary/90 transition cursor-pointer"
       >
-        Back to Home Page
-      </a>
+        Go Back
+      </button>
     </main>
   );
 }
