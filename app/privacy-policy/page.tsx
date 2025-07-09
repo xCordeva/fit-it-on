de494203fe5d44a4486d630683692535d0105cc8
Ahmad AbdEl-Aal <ahmad.abdelaallabas@gmail.com>
2025-07-09 07:10:10 +0300

    chore: remove use client and import react

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,7 +1,3 @@
-"use client";
-
-import React from "react";
-
 export default function PrivacyPolicy() {
   return (
     <main className="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-md my-10">
