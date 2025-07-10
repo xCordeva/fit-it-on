59ff3fb084ac9d2c7fc55f79874293db2290c184
Ahmad AbdEl-Aal <ahmad.abdelaallabas@gmail.com>
2025-07-11 01:35:08 +0300

    chore: change lemon squeezy links

--- lib/payments.ts
+++ lib/payments.ts
@@ -6,7 +6,7 @@ export const PLANS: Record<PlanKey, Plan> = {
     price: "4.99",
     credits: 50,
     checkoutUrl:
-      "https://fititon.lemonsqueezy.com/buy/4ab6a6f9-55ca-4219-9102-09676dc7bfeb?media=0&discount=0",
+      "https://fititon.lemonsqueezy.com/buy/4ae924a7-c78e-4856-948e-81ee6291c6c8?media=0&discount=0",
     productId: 570814,
     features: [
       "50 try-on images per month",
@@ -22,7 +22,7 @@ export const PLANS: Record<PlanKey, Plan> = {
     price: "8.99",
     credits: 100,
     checkoutUrl:
-      "https://fititon.lemonsqueezy.com/buy/dd624191-370a-4beb-b303-030656f26c66?media=0&discount=0",
+      "https://fititon.lemonsqueezy.com/buy/e80ddf37-7e09-465e-86c2-73296d9c20d1?media=0&discount=0",
     productId: 570816,
     features: [
       "100 try-on images per month",
@@ -37,7 +37,7 @@ export const PLANS: Record<PlanKey, Plan> = {
     price: "18.99",
     credits: 250,
     checkoutUrl:
-      "https://fititon.lemonsqueezy.com/buy/a4163b0f-5c76-4041-a1da-ddecca9279e8?media=0&discount=0",
+      "https://fititon.lemonsqueezy.com/buy/91ee8a37-fb83-497d-b5ef-9a18fcb1af18?media=0&discount=0",
     productId: 570817,
     features: [
       "250 try-on images per month",
