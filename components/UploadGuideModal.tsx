055eee565db1f06f5f93ba615d7236c9f7e4da1e
Ahmad AbdEl-Aal <ahmad.abdelaallabas@gmail.com>
2025-08-22 20:02:20 +0300

    chore: change upload guid photos

--- components/UploadGuideModal.tsx
+++ components/UploadGuideModal.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
-import { FaCircleXmark } from "react-icons/fa6";
-import { IoIosCheckmarkCircle } from "react-icons/io";
 import Image from "next/image";
 
 interface UploadGuideModalProps {
@@ -16,102 +14,28 @@ export default function UploadGuideModal({
 }: UploadGuideModalProps) {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="sm:max-w-lg max-h-[90vh] overflow-y-auto flex flex-col rounded-lg">
+      <DialogContent className="sm:max-w-4xl overflow-y-auto flex flex-col rounded-lg">
         <DialogTitle className="hidden"></DialogTitle>
-        <div className="flex items-center justify-center mx-auto flex-col max-w-120 overflow-hidden">
-          <div>
-            <h2 className="text-green-500 flex items-center justify-center gap-1">
-              <IoIosCheckmarkCircle />
-              Suitable Photos
-            </h2>
-            <p className="text-gray-500 text-center">
-              For the best result, please use a photo with a white background, a
-              single item in front view, and a clear, simple pattern.
-            </p>
-            <div className="flex gap-2 items-center justify-center mt-2 flex-wrap">
-              <Image
-                height={80}
-                width={80}
-                src="/suitable-photos/coat.webp"
-                alt="fit-it-on-f-logo"
-                className="border-1 border-gray-500 p-2"
-              />
-              <Image
-                height={80}
-                width={80}
-                src="/suitable-photos/sweat-shirt.webp"
-                alt="fit-it-on-f-logo"
-                className="border-1 border-gray-500 p-2"
-              />
-              <Image
-                height={80}
-                width={80}
-                src="/suitable-photos/jacket.webp"
-                alt="fit-it-on-f-logo"
-                className="border-1 border-gray-500 p-2 object-cover w-20 h-20"
-              />
-              <Image
-                height={80}
-                width={80}
-                src="/suitable-photos/dress.webp"
-                alt="fit-it-on-f-logo"
-                className="border-1 border-gray-500 p-2"
-              />
-              <Image
-                height={80}
-                width={80}
-                src="/suitable-photos/t-shirt.webp"
-                alt="fit-it-on-f-logo"
-                className="border-1 border-gray-500 p-2"
-              />
-            </div>
+        <div className="flex items-center justify-center mx-auto flex-col max-w-6xl gap-6">
+          <div className="relative w-full h-auto">
+            <Image
+              src="/suitable-photos/person-photo-type.webp"
+              alt="Person photo type guide"
+              width={1200}
+              height={0}
+              sizes="100vw"
+              className="border border-gray-500 p-2 h-auto w-full"
+            />
           </div>
-          <div className="mt-4">
-            <h2 className="text-red-500 flex items-center justify-center gap-1">
-              <FaCircleXmark />
-              Unsuitable Photos
-            </h2>
-            <p className="text-gray-500 text-center">
-              Avoid using photos with multiple items, busy backgrounds or
-              patterns, text overlays, folds, or any obstructions.
-            </p>
-            <div className="flex gap-2 items-center justify-center mt-2 flex-wrap">
-              <Image
-                height={80}
-                width={80}
-                src="/unsuitable-photos/cover.webp"
-                alt="fit-it-on-f-logo"
-                className="border-1 border-gray-500 p-0 object-cover w-20 h-20"
-              />
-              <Image
-                height={80}
-                width={80}
-                src="/unsuitable-photos/2-looks.webp"
-                alt="fit-it-on-f-logo"
-                className="border-1 border-gray-500 p-0  object-cover w-20 h-20"
-              />
-              <Image
-                height={80}
-                width={80}
-                src="/unsuitable-photos/back-dress.webp"
-                alt="fit-it-on-f-logo"
-                className="border-1 border-gray-500 p-0 object-cover"
-              />
-              <Image
-                height={80}
-                width={80}
-                src="/unsuitable-photos/models.webp"
-                alt="fit-it-on-f-logo"
-                className="border-1 border-gray-500 p-0  object-cover w-20 h-20"
-              />
-              <Image
-                height={80}
-                width={80}
-                src="/unsuitable-photos/multi-items.webp"
-                alt="fit-it-on-f-logo"
-                className="border-1 border-gray-500 p-2"
-              />
-            </div>
+          <div className="relative w-full h-auto">
+            <Image
+              src="/suitable-photos/garment-photo-type.webp"
+              alt="Garment photo type guide"
+              width={1200}
+              height={0}
+              sizes="100vw"
+              className="border border-gray-500 p-2 h-auto w-full"
+            />
           </div>
         </div>
       </DialogContent>
